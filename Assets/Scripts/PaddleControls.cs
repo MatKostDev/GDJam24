434a5749f9017b8ada4382b89c534e5e587ea2d4
Mathew Kostrzewa <mathew.kostrzewa@ontariotechu.net>
2024-11-03 02:54:43 -0500

    She kinda goes

--- Assets/Scripts/PaddleControls.cs
+++ Assets/Scripts/PaddleControls.cs
@@ -2,30 +2,51 @@ using UnityEngine;
 
 public class PaddleControls : MonoBehaviour
 {
+    [Header("Paddle Movement Range")]
     [SerializeField]
     float paddleDynamicSideOffset = 0.6f;
 
     [SerializeField]
     float paddleDynamicHeightOffset = 0.25f;
 
+    [Header("Forces")]
+    [SerializeField]
+    float paddleForwardForceMulti = 70f;
+
+    [SerializeField]
+    float paddleUpwardForceMulti = 50f;
+
+    [SerializeField]
+    float flipCanoeForceMulti = 400f;
+
+    [Header("Transforms")]
     [SerializeField]
     Transform canoeTransform;
 
+    [SerializeField]
+    Transform paddleContactTransform;
+
     float m_paddleStaticHeightOffset;
+
+    Vector2 m_lastRelativeScreenPos;
 
     void Start()
     {
         m_paddleStaticHeightOffset = transform.localPosition.y;
+
+        m_lastRelativeScreenPos = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
     }
 
     void Update()
     {
-        bool    clickHeld = Input.GetMouseButton(0) || Input.GetMouseButton(1);
-        Vector3 mousePos  = Input.mousePosition;
+        bool    flipCanoe  = Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.Space);
+        bool    clickFrame = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
+        bool    clickHeld  = Input.GetMouseButton(0)     || Input.GetMouseButton(1);
+        Vector3 mousePos   = Input.mousePosition;
 
         Vector2 relativeScreenPos = new Vector2(mousePos.x / Screen.width, mousePos.y / Screen.height);
-        relativeScreenPos.x = relativeScreenPos.x * 2f - 1f;
-        relativeScreenPos.y = relativeScreenPos.y * 2f - 1f;
+        relativeScreenPos.x = Mathf.Clamp(relativeScreenPos.x * 2f - 1f, -1f, 1f);
+        relativeScreenPos.y = Mathf.Clamp((relativeScreenPos.y * 2f - 1f) * 2f, -1f, 1f);
 
         int sideMulti   = relativeScreenPos.x > 0f ? 1 : -1;
         int heightMulti = clickHeld ? -1 : 1;
@@ -41,13 +62,33 @@ public class PaddleControls : MonoBehaviour
         newPos += canoeTransform.up      * (paddleDynamicHeightOffset * heightMulti + m_paddleStaticHeightOffset);
         newPos += canoeTransform.forward * relativeScreenPos.y * 1.2f;
 
-        float deltaZ = transform.position.z - newPos.z;
+        float paddleSpeed = m_lastRelativeScreenPos.y - relativeScreenPos.y;
 
         transform.position = newPos;
 
         if (clickHeld)
         {
-            canoeTransform.GetComponent<Rigidbody>().AddForceAtPosition(canoeTransform.forward * deltaZ * Time.deltaTime * 30000f, transform.position - new Vector3(0f, -0.5f, 0f));
+            Vector3 forwardForce = paddleForwardForceMulti * paddleSpeed * canoeTransform.forward;
+
+            canoeTransform.GetComponent<Rigidbody>().AddForceAtPosition(forwardForce, paddleContactTransform.position);
         }
+        if (clickFrame)
+        {
+            Vector3 upwardForce = canoeTransform.up * paddleUpwardForceMulti;
+
+            Vector3 forceOrigin = canoePos + canoeTransform.forward * transform.localPosition.z;
+
+            canoeTransform.GetComponent<Rigidbody>().AddForceAtPosition(upwardForce, forceOrigin);
+        }
+        if (flipCanoe)
+        {
+            Vector3 flipForce = Vector3.up * flipCanoeForceMulti;
+
+            Vector3 forceOrigin = canoePos + canoeTransform.right * transform.localPosition.x;
+
+            canoeTransform.GetComponent<Rigidbody>().AddForceAtPosition(flipForce, forceOrigin);
+        }
+
+        m_lastRelativeScreenPos = relativeScreenPos;
     }
 }
